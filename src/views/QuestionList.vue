1ac29c285f781131c0c3d8709ff9ddf2e321c0e6
nht-dilshan <tharindudilshanem6263@gmail.com>
2025-03-22 15:57:38 +0530

    Increase timer duration in QuestionList component from 6 seconds to 600 seconds for improved user experience.

--- src/views/QuestionList.vue
+++ src/views/QuestionList.vue
@@ -32,7 +32,7 @@ export default {
       currentQuestion: 0,
       userAnswer: '',
       totalScore: 0,
-      timer: 6, // 5 minutes in seconds
+      timer: 600, // 5 minutes in seconds
       timerInterval: null,
       questions: [
         {
